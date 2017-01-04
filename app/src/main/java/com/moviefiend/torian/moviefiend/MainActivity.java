0819dec39f63c572688a4588999f29dfc24c23ec
tomerorian <gogofo+github@gmail.com>
2017-01-04 13:42:59 +0200

    project creation

---
+++ app/src/main/java/com/moviefiend/torian/moviefiend/MainActivity.java
@@ -0,0 +1,13 @@
+package com.moviefiend.torian.moviefiend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
