32b04dfe40a8b9c781a85a8b3e5841d316f8f9d7
tomerorian <gogofo+github@gmail.com>
2017-01-10 16:29:54 +0200

    extract string

--- app/src/main/java/com/moviefiend/torian/moviefiend/MovieViewHolder.java
+++ app/src/main/java/com/moviefiend/torian/moviefiend/MovieViewHolder.java
@@ -1,14 +1,11 @@
 package com.moviefiend.torian.moviefiend;
 
-import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
-
-import java.util.Locale;
 
 public class MovieViewHolder extends RecyclerView.ViewHolder {
 
@@ -37,6 +34,6 @@ public class MovieViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void setRating(float rating) {
-        mRatingView.setText(String.format(Locale.US, "Rating: %.1f / 10", rating));
+        mRatingView.setText(itemView.getContext().getString(R.string.rating_lable, rating));
     }
 }
