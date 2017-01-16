4fa56210da97bdfcd016fd90b962bb96c40473fe
tomerorian <gogofo+github@gmail.com>
2017-01-16 11:28:40 +0200

    fill the details, add scrollview

--- app/src/main/java/com/moviefiend/torian/moviefiend/MovieDetailsFragment.java
+++ app/src/main/java/com/moviefiend/torian/moviefiend/MovieDetailsFragment.java
@@ -6,12 +6,19 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.moviefiend.torian.moviefiend.network.NowPlayingResponse;
+import com.squareup.picasso.Picasso;
 
 public class MovieDetailsFragment extends Fragment {
 
     private NowPlayingResponse.MovieInfo mMovieInfo;
+    private TextView mTitleView;
+    private ImageView mPosterView;
+    private TextView mRatingView;
+    private TextView mDescriptionView;
 
     @Nullable
     @Override
@@ -20,6 +27,34 @@ public class MovieDetailsFragment extends Fragment {
 
         mMovieInfo = getArguments().getParcelable("movie_info");
 
+        mTitleView = (TextView) view.findViewById(R.id.title);
+        mPosterView = (ImageView) view.findViewById(R.id.poster);
+        mRatingView = (TextView) view.findViewById(R.id.rating);
+        mDescriptionView = (TextView) view.findViewById(R.id.description);
+
+        setTitle(mMovieInfo.getTitle());
+        setPosterView(getString(R.string.tmdb_poster_url, mMovieInfo.getPosterPath()));
+        setRating(mMovieInfo.getRating());
+        setDescription("TODO TODO TODO");
+
         return view;
+    }
+
+    private void setTitle(String title) {
+        mTitleView.setText(title);
+    }
+
+    private void setPosterView(String url) {
+        Picasso.with(getActivity())
+                .load(url)
+                .into(mPosterView);
+    }
+
+    private void setRating(float rating) {
+        mRatingView.setText(getString(R.string.rating_lable, rating));
+    }
+
+    private void setDescription(String description) {
+        mDescriptionView.setText(description);
     }
 }
