2afd30f4d2d845dfa3722b6788a2f7b237771569
tomerorian <gogofo+github@gmail.com>
2017-01-16 13:54:08 +0200

    add similar movies activity + link it

--- app/src/main/java/com/moviefiend/torian/moviefiend/MovieDetailsActivity.java
+++ app/src/main/java/com/moviefiend/torian/moviefiend/MovieDetailsActivity.java
@@ -1,10 +1,13 @@
 package com.moviefiend.torian.moviefiend;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-public class MovieDetailsActivity extends AppCompatActivity {
+import com.moviefiend.torian.moviefiend.network.MoviesResponse;
+
+public class MovieDetailsActivity extends AppCompatActivity implements MovieDetailsFragment.Listener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,11 +15,18 @@ public class MovieDetailsActivity extends AppCompatActivity {
 
         MovieDetailsFragment fragment = new MovieDetailsFragment();
         fragment.setArguments(getIntent().getExtras());
+        fragment.setListener(this);
 
         getFragmentManager()
                 .beginTransaction()
                 .add(R.id.content_fragment, fragment)
                 .commit();
 
+    }
+
+    @Override
+    public void onSimilarMoviesClicked(MoviesResponse.MovieInfo movieInfo) {
+        Intent intent = new Intent(this, SimilarMoviesActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/moviefiend/torian/moviefiend/MovieDetailsFragment.java
+++ app/src/main/java/com/moviefiend/torian/moviefiend/MovieDetailsFragment.java
@@ -14,12 +14,18 @@ import com.squareup.picasso.Picasso;
 
 public class MovieDetailsFragment extends Fragment {
 
+    public interface Listener {
+        void onSimilarMoviesClicked(MoviesResponse.MovieInfo movieInfo);
+    }
+
     public static final int MOVIE_POSTER_SIZE = 780;
 
     private MoviesResponse.MovieInfo mMovieInfo;
     private ImageView mPosterView;
     private TextView mRatingView;
+    private TextView mSimilarMoviesBtn;
     private TextView mDescriptionView;
+    private Listener mListener;
 
     @Nullable
     @Override
@@ -31,13 +37,27 @@ public class MovieDetailsFragment extends Fragment {
         mPosterView = (ImageView) view.findViewById(R.id.poster);
         mRatingView = (TextView) view.findViewById(R.id.rating);
         mDescriptionView = (TextView) view.findViewById(R.id.description);
+        mSimilarMoviesBtn = (TextView) view.findViewById(R.id.similar_movies);
 
         setTitle(mMovieInfo.getTitle());
         setPosterView(getString(R.string.tmdb_poster_url, MOVIE_POSTER_SIZE, mMovieInfo.getPosterPath()));
         setRating(mMovieInfo.getRating());
         setDescription(mMovieInfo.getDescription());
 
+        mSimilarMoviesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mListener != null) {
+                    mListener.onSimilarMoviesClicked(mMovieInfo);
+                }
+            }
+        });
+
         return view;
+    }
+
+    public void setListener(Listener listener) {
+        mListener = listener;
     }
 
     private void setTitle(String title) {
