956d25fde7d74e6790523eb168fb27fdca459134
tomerorian <gogofo+github@gmail.com>
2017-01-17 10:40:33 +0200

    create the view pager and adapter
    currently loading all movies and not similar, will impl that next
    there is also seems to be a bug with the title to handle with

--- app/src/main/java/com/moviefiend/torian/moviefiend/SimilarMoviesActivity.java
+++ app/src/main/java/com/moviefiend/torian/moviefiend/SimilarMoviesActivity.java
@@ -2,12 +2,91 @@ package com.moviefiend.torian.moviefiend;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
-public class SimilarMoviesActivity extends AppCompatActivity {
+import com.moviefiend.torian.moviefiend.network.MoviesResponse;
+
+import java.util.ArrayList;
+
+public class SimilarMoviesActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<ArrayList<MoviesResponse.MovieInfo>> {
+
+    private static final int MOVIE_LOADER_ID = 0;
+
+    private ViewPager mMoviePager;
+    private MoviePagerAdapter mMoviePagerAdapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.similar_movies_activity);
+
+        mMoviePagerAdapter = new MoviePagerAdapter(getSupportFragmentManager());
+
+        mMoviePager = (ViewPager) findViewById(R.id.movie_pager);
+        mMoviePager.setAdapter(mMoviePagerAdapter);
+
+        getSupportLoaderManager().initLoader(MOVIE_LOADER_ID, null, this);
+    }
+
+    //<editor-fold desc="Loader">
+    @Override
+    public Loader<ArrayList<MoviesResponse.MovieInfo>> onCreateLoader(int id, Bundle args) {
+        return new MovieInfoLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<ArrayList<MoviesResponse.MovieInfo>> loader,
+                               ArrayList<MoviesResponse.MovieInfo> data) {
+        mMoviePagerAdapter.setMovies(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<ArrayList<MoviesResponse.MovieInfo>> loader) {
+
+    }
+    //</editor-fold>
+
+    private static class MoviePagerAdapter extends FragmentStatePagerAdapter {
+
+        private ArrayList<MoviesResponse.MovieInfo> mMovies;
+
+        public MoviePagerAdapter(FragmentManager fm) {
+            super(fm);
+
+            mMovies = new ArrayList<>();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            MovieDetailsFragment fragment = new MovieDetailsFragment();
+
+            Bundle arguments = new Bundle();
+            arguments.putParcelable(MovieDetailsFragment.MOVIE_INFO_ARG, mMovies.get(position));
+            fragment.setArguments(arguments);
+
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return mMovies.size();
+        }
+
+        public void setMovies(ArrayList<MoviesResponse.MovieInfo> movies) {
+            if (movies != null) {
+                this.mMovies = movies;
+            } else {
+                this.mMovies = new ArrayList<>();
+            }
+
+            notifyDataSetChanged();
+        }
     }
 }
