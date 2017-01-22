ec16ef8941804ebd8a4ccbbd5b45e04baae3fe85
tomerorian <gogofo+github@gmail.com>
2017-01-22 11:20:08 +0200

    support backstack for movie details in deep link

--- app/src/main/java/com/moviefiend/torian/moviefiend/MovieDetailsLoaderActivity.java
+++ app/src/main/java/com/moviefiend/torian/moviefiend/MovieDetailsLoaderActivity.java
@@ -5,8 +5,10 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.moviefiend.torian.moviefiend.network.MovieInfo;
 
@@ -40,7 +42,11 @@ public class MovieDetailsLoaderActivity extends AppCompatActivity implements Loa
         if (data != null) {
             Intent intent = new Intent(this, MovieDetailsActivity.class);
             intent.putExtra(MovieDetailsActivity.MOVIE_INFO_EXTRA, data);
-            startActivity(intent);
+
+            TaskStackBuilder.create(this)
+                    .addNextIntentWithParentStack(intent)
+                    .startActivities();
+
             finish();
         } else {
             onFail();
